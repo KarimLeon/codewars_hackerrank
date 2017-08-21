106973a5b3ff8c9f059c06b1516db85d1dd8fd54
KarimLeon <leon.karim08@gmail.com>
2017-08-21 02:30:24 -0400

    edit

--- sumCubedOddsNumbers.js
+++ sumCubedOddsNumbers.js
@@ -1,23 +1,21 @@
 function cubeOdd(arr) {
-var array = [], arr1 = [], sum = 0
-for(var i = 0; i < arr.length; i++) { 
-     if(arr[i] % 2 !== 0)
-        var a = array.push(arr[i]);
-
-};
-
-for(var x = 0; x < array.length; x++) {
-       var g = Math.pow(array[x], 3);
-       var lol = arr1.push(g);
-
-}
-
-for(var b = 0; b < arr1.length; b++) {
-       sum += arr1[b];
-   
-};
-
-return sum;
-return 0;
-
-};
+    var array = [], arr1 = [], sum = 0, y = ""
+    
+    for(var i = 0; i < arr.length; i++) {    
+         if(arr[i] % 2 !== 0)
+            var a = array.push(arr[i]);
+    };
+    
+    for(var x = 0; x < array.length; x++) {
+           var g = Math.pow(array[x], 3);
+           var lol = arr1.push(g);
+    }
+    
+    for(var b = 0; b < arr1.length; b++) {
+           sum += arr1[b];
+    };
+    
+    return sum;
+    return 0;
+    
+    };
