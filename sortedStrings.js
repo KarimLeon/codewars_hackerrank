bbd40f30bf77e03ecfc8c22a6a85bbe9204425d9
KarimLeon <leon.karim08@gmail.com>
2017-08-22 04:06:59 -0400

    Unsolved.. Still attacking problem!

---
+++ sortedStrings.js
@@ -0,0 +1,15 @@
+function longest(s1, s2) {
+    var unity = s1 + s2, letters = 'abcdefghijklmopqrstuvwxyz', 
+    array = letters.split('');
+
+  for(var x = 0; x < array.length; x++) {
+  for(var i = 0; i < unity.length; i++) {
+        if(unity[i] == array[x])
+          var sorted = unity[i];
+      
+}
+}
+return sorted;
+
+
+};
