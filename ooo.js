69a0231d38234ed902a6b11b87ec3aa3d11c15bd
KarimLeon <leon.karim08@gmail.com>
2017-08-24 18:57:01 -0400

    test

---
+++ ooo.js
@@ -0,0 +1,30 @@
+var people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+    
+var admitted = document.querySelector('.admitted');
+var refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: '
+
+ for(var i = 0; i < people.length; i++) {
+       switch(people[i]) {
+     
+       case 'Phil':
+         admitted.textContent += 'Phil' + ', '.replace(',', '')
+         break;
+  
+       case 'Lola':
+         admitted.textContent += 'Lola'
+         break; 
+
+        case people[i] :
+          refused.textContent += people[i] + ', '.replace(',','')
+           break;
+        
+}
+
+};
+
+
+
+
+
