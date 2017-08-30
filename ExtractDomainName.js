4ec4d1df4fb44f6a3c8273b4861f59c3ca79fed3
KarimLeon <leon.karim08@gmail.com>
2017-08-30 02:45:27 -0400

    !done

---
+++ ExtractDomainName.js
@@ -0,0 +1,28 @@
+function domainName(url){
+
+  var a = url.lastIndexOf('/') + 1;
+  var b = url.indexOf('.') + 1;
+
+var rofl = function () {
+  var g = url.slice(a); 
+  var lol = g.indexOf('.') 
+  var ok = g.slice(0, lol)
+return ok;
+  };
+
+var gay = function () {
+  var h = url.slice(b)
+  var hh = h.indexOf('.')
+  var ggg = h.slice(0, hh);
+return ggg;
+};
+
+if(url.lastIndexOf('/') !== -1 && url.lastIndexOf('/') === -1) 
+     var x = gay();
+ else
+     x =  rofl();
+
+  
+return x;
+
+};
